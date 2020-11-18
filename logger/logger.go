ff4715433ffd00101a2f4bec688e01663a4f90c7
GZShi <vip@eval.im>
2020-11-18 10:26:49 +0800

    优化常见的错误，减少日志体积

--- logger/logger.go
+++ logger/logger.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/shiena/ansicolor"
 	"github.com/sirupsen/logrus"
@@ -10,12 +12,12 @@ import (
 var gLogger = logrus.New()
 
 func init() {
-	gLogger.Formatter = &logrus.TextFormatter{
+	gLogger.Formatter = &errFormatter{&logrus.TextFormatter{
 		ForceColors:      true,
 		DisableTimestamp: false,
 		FullTimestamp:    true,
 		TimestampFormat:  "2006-01-02T15:04:05.9999",
-	}
+	}}
 
 	gLogger.Out = ansicolor.NewAnsiColorWriter(os.Stdout)
 	gLogger.Level = logrus.DebugLevel
@@ -24,4 +26,24 @@ func init() {
 // Get 获取实例
 func Get() *logrus.Logger {
 	return gLogger
+}
+
+type errFormatter struct {
+	Formatter *logrus.TextFormatter
+}
+
+// Format 将error字段进行格式化
+func (f *errFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	// 对错误信息进行简化
+	val, ok := entry.Data[logrus.ErrorKey]
+	if ok {
+		str := fmt.Sprint(val)
+		const errEstablishFailed = "A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond."
+		switch {
+		case strings.HasSuffix(str, errEstablishFailed):
+			entry.Data[logrus.ErrorKey] = "errEstablishFailed"
+		}
+	}
+
+	return f.Formatter.Format(entry)
 }
