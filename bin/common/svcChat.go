e49c304035c4a63de3585b365d718b10ae149cfe
GZShi <vip@eval.im>
2021-05-21 20:55:21 +0800

    调整界面服务参数

--- bin/common/svcChat.go
+++ bin/common/svcChat.go
@@ -3,6 +3,7 @@ package common
 import (
 	"io"
 	"net"
+	"time"
 
 	"github.com/GZShi/net-agent/rpc/cluster/def"
 	"github.com/GZShi/net-agent/rpc/msgclient"
@@ -15,13 +16,14 @@ import (
 func response(ctx iris.Context, data interface{}, err error) {
 	if err != nil {
 		ctx.JSON(&struct {
-			ErrCode int    `json:"errCode"`
-			ErrMsg  string `json:"errMsg"`
+			ErrCode int
+			ErrMsg  string
 		}{-1, err.Error()})
+		return
 	}
 	ctx.JSON(&struct {
-		ErrCode int    `json:"errCode"`
-		ErrMsg  string `json:"errMsg"`
+		ErrCode int
+		ErrMsg  string
 		Data    interface{}
 	}{0, "success", data})
 }
@@ -41,21 +43,33 @@ func RunChatServer(t tunnel.Tunnel, cls def.Cluster, param map[string]string, lo
 	t.BindServices(svc)
 
 	app := iris.New()
-	app.StaticWeb("/chat", "./chat-html")
+	app.StaticWeb("/example", "./ui-html/example")
+	app.StaticWeb("/site", "./ui-html/site")
 	app.Get("/say-hello", func(ctx iris.Context) {
 		ctx.Write([]byte("who you are~ ?"))
 	})
-	app.Post("/new-message", func(ctx iris.Context) {
-		var msg struct {
-			GroupID uint32 `json:"groupID"`
-			MsgType int    `json:"msgType"`
-			Message string `json:"message"`
-		}
-		ctx.ReadJSON(&msg)
+	api := app.Party("/agent-api")
+	{
+		api.Get("/ctx-info", func(ctx iris.Context) {
+			info, err := cls.GetCtxInfo()
+			response(ctx, &info, err)
+		})
+		api.Post("/new-message", func(ctx iris.Context) {
+			var msg struct {
+				GroupID uint32 `json:"groupID"`
+				MsgType int    `json:"msgType"`
+				Message string `json:"message"`
+			}
+			ctx.ReadJSON(&msg)
 
-		err := cls.SendGroupMessage(msg.GroupID, msg.Message, msg.MsgType)
-		response(ctx, nil, err)
-	})
+			err := cls.SendGroupMessage(msg.GroupID, msg.Message, msg.MsgType)
+			response(ctx, nil, err)
+		})
+		api.Post("/recent-message", func(ctx iris.Context) {
+			msgs, err := cls.GetGroupMessages([]uint32{0}, time.Now().Add(-7*24*time.Hour), 100)
+			response(ctx, msgs, err)
+		})
+	}
 
 	// 设置websocket接口
 	ws := websocket.New(websocket.Config{
